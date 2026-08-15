63b9ed7feb5d2bd292b957c15d61968991c89901
alsabai2004 <mhmdnjyb085@gmail.com>
2026-08-15 05:36:05 +0300

    Polish Web-HireU navigation

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,11 +1,15 @@
-<link rel="stylesheet" href="/css/web-hireu.css">
-<nav>
-<a href="/">Web-HireU</a> |
-<a href="/jobs">Jobs</a> |
-<a href="/categories">Categories</a> |
-<a href="/dashboard">Dashboard</a> |
-<a href="/applications">My Applications</a> |
-<a href="/create-job">Post Job</a> |
-<a href="/logout">Logout</a>
+<nav class="navbar">
+    <div class="nav-container">
+        <a class="brand" href="/">Web-HireU</a>
+
+        <div class="nav-links">
+            <a href="/jobs">Jobs</a>
+            <a href="/categories">Categories</a>
+            <a href="/search">Search</a>
+            <a href="/dashboard">Dashboard</a>
+            <a href="/applications">My Applications</a>
+            <a href="/create-job">Post Job</a>
+            <a href="/logout">Logout</a>
+        </div>
+    </div>
 </nav>
-<hr>
